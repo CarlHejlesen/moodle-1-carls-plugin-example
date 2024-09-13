655447a6167498e599422676a3ab944f59de8cbb
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2024-09-13 22:09:27 +0200

    Git ignore skal tilføjes

---
+++ server/moodle/local/message/lang/en/local_message.php
@@ -0,0 +1,5 @@
+<?php
+$string['pagetitle'] = 'My Custom Page';
+$string['pageheading'] = 'Velkommen til carls side';
+$string['welcomeheading'] = 'Hej, Moodle!';
+$string['welcomemessage'] = 'This is a custom page created to demonstrate creating a new page in Moodle.';
